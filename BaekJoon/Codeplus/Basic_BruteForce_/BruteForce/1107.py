e701702e77feb4508eed0ce88923831a90eb97aa
Gwanwoo Shin <tlsrhksdn23@gmail.com>
2023-02-13 11:33:15 +0000

    2/13 공부기록 수학영역 복습 17427번 해결 174251번 해결 1929번 해결

---
+++ BaekJoon/Codeplus/Basic_BruteForce_/BruteForce/1107.py
@@ -0,0 +1,20 @@
+numbers = [i for i in range(10)]
+channel_number = 100
+
+N = int(input())
+number = int(input())
+broken_numbers = []
+cnt = 0
+
+for i in range(number):
+    broken_numbers.apppend()
+
+for i in range(len(broken_numbers)):
+  numbers.remove(broken_numbers[i])
+
+
+while channel_number == N:
+  
+
+
+print(cnt)
