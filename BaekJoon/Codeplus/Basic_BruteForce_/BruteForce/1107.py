4f2452751af95d09a9465129df37d9c88c2a35ff
Gwanwoo Shin <tlsrhksdn23@gmail.com>
2023-02-14 12:09:08 +0000

    2/14 공부기록 1107번, 6064번 도전(실패..)

--- BaekJoon/Codeplus/Basic_BruteForce_/BruteForce/1107.py
+++ BaekJoon/Codeplus/Basic_BruteForce_/BruteForce/1107.py
@@ -2,19 +2,24 @@ numbers = [i for i in range(10)]
 channel_number = 100
 
 N = int(input())
-number = int(input())
-broken_numbers = []
-cnt = 0
+M = int(input())
 
-for i in range(number):
-    broken_numbers.apppend()
+broken_numbers = [int(input()) for _ in range(M)]
 
-for i in range(len(broken_numbers)):
+for i in range(M):
   numbers.remove(broken_numbers[i])
 
+min_cnt = abs(100 - N)
 
 while channel_number == N:
-  
+  for i in range(1000000):
+    num = str(i)
+    if i == len(num-1):
+      
+
+    
+
+    
 
 
 print(cnt)
