cea4ecaf6c951589f685c6f85b28ae64fbb8439d
Gwanwoo Shin <tlsrhksdn23@gmail.com>
2023-02-15 12:01:02 +0000

    2/15 공부기록 6064번 해결

--- BaekJoon/Codeplus/Basic_BruteForce_/BruteForce/6064.py
+++ BaekJoon/Codeplus/Basic_BruteForce_/BruteForce/6064.py
@@ -1,38 +1,18 @@
+def getcount(m,n,x,y):
+  while x <= m * n:
+    if (x-y) % n == 0:
+      return x
+    x += m
+
+  return -1
+  
 T = int(input())
 
 
 for i in range(T):
-  M, N, x, y = map(int, input().split())
+  m, n, x, y = map(int, input().split())
 
-  cnt = 0
-
-  x_ = 0
-  y_ = 0
-
-  while True:
-    
-    if(x > y and M < N and x - y > N - M):
-      print(-1)
-      break
-      
-    elif(x < y and M > N and y - x > M - N):
-      print(-1)
-      break       
-      
-    else:
-      if(x_ == x and y_ == y):
-        print(cnt)
-        break
-      else:
-    
-        x_ += 1
-        y_ += 1
-        cnt += 1
-
-        if x_ >= M:
-          x_ %= M
-        elif y_ >= N:
-          y_ %= N
+  print(getcount(m,n,x,y))
 
         
 
